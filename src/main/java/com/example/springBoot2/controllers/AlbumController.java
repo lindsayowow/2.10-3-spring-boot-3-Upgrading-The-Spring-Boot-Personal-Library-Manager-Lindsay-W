24ef19ed156d3c9135a045dd3ccbb1eedf566fd6
lindsayowow <lindsayow@gmail.com>
2026-07-01 20:27:10 -0500

    updated controllers

--- src/main/java/com/example/springBoot2/controllers/AlbumController.java
+++ src/main/java/com/example/springBoot2/controllers/AlbumController.java
@@ -1,6 +1,7 @@
 package com.example.springBoot2.controllers;
 
 import com.example.springBoot2.models.Album;
+import com.example.springBoot2.repositories.AlbumRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -8,14 +9,9 @@ import java.util.List;
 @RestController
 @RequestMapping("/albums")
 public class AlbumController {
-    private final List<Album> albums = List.of(
-        new Album("The Dark Side of the Moon", "Pink Floyd", 1973, 10),
-        new Album("Back in Black", "AC/DC", 1980, 10),
-        new Album("The Bodyguard", "Whitney Houston", 1992, 10)
-    );
+    private final AlbumRepository albumRepository;
 
-    @GetMapping
-    public List<Album> getAlbums() {
-        return albums;
+    public AlbumController(AlbumRepository albumRepository) {
+        this.albumRepository = albumRepository;
     }
 }
--- src/main/java/com/example/springBoot2/controllers/MovieController.java
+++ src/main/java/com/example/springBoot2/controllers/MovieController.java
@@ -1,6 +1,8 @@
 package com.example.springBoot2.controllers;
 
 import com.example.springBoot2.models.Movie;
+import com.example.springBoot2.repositories.BookRepository;
+import com.example.springBoot2.repositories.MovieRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -8,14 +10,9 @@ import java.util.List;
 @RestController
 @RequestMapping("/movies")
 public class MovieController {
-    private final List<Movie> movies = List.of(
-        new Movie("The Shawshank Redemption", "Frank Darabont", 1994, 142),
-        new Movie("The Godfather", "Francis Ford Coppola", 1972, 175),
-        new Movie("The Dark Knight", "Christopher Nolan", 2008, 152)
-    );
+    private final MovieRepository movieRepository;
 
-    @GetMapping
-    public List<Movie> getMovies() {
-        return movies;
+    public MovieController(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
     }
 }
